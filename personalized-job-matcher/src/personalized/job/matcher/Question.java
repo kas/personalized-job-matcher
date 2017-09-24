5c7f855bc33a91f552ef436662a7f66528ed0729
elimack3 <32169132+elimack3@users.noreply.github.com>
2017-09-24 13:03:14 -0400

    Added tests.

--- personalized-job-matcher/src/personalized/job/matcher/Question.java
+++ personalized-job-matcher/src/personalized/job/matcher/Question.java
@@ -8,14 +8,35 @@ public class Question {
     private int questionID = 0;
     private String questionText = " ";
     ArrayList<Choice> choices = new ArrayList<>();
+    private Choice Choice2;
+    private Choice Choice1;
+    private Choice Choice3;
+    private Choice Choice4;
     
-    public Question(int newQuestionID, String newQuestionText, ArrayList newChoices){
+    
+    public Question(int newQuestionID, String newQuestionText){
         
         this.questionID = newQuestionID;
         this.questionText = newQuestionText;
-        this.choices = newChoices;
+        makeChoices();
+        addChoices();
     }
-
+    
+    public void makeChoices(){
+        Choice1 = new Choice(1, "Red");
+        Choice2 = new Choice(2, "Blue");
+        Choice3 = new Choice(3, "Yellow");
+        Choice4 = new Choice(4, "Green");
+    }
+    
+    
+    public void addChoices(){
+        choices.add(Choice1);
+        choices.add(Choice2);
+        choices.add(Choice3);
+        choices.add(Choice4);
+    }
+    
     public int getQuestionID() {
         return questionID;
     }
@@ -33,6 +54,6 @@ public class Question {
     }
     
     public void getQuestionInfo(){
-        System.out.println(questionID + questionText);
+        System.out.println(questionID + " " + questionText);
     }
 }
