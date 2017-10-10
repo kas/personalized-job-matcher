4a00a48d48ad774da2d0bbf5c9b06fa9366ffe2f
eem5034 <eem5034@E165-WL10.up.ist.local>
2017-10-10 09:57:13 -0400

    Question class updated.

--- personalized-job-matcher/src/personalized/job/matcher/JobSeekerController.java
+++ personalized-job-matcher/src/personalized/job/matcher/JobSeekerController.java
@@ -3,5 +3,7 @@ package personalized.job.matcher;
 
 
 public class JobSeekerController {
-    
+    public JobSeekerController(){
+        JobSeekerProfileUI jsProfilePage = new JobSeekerProfileUI();
+    }
 }
