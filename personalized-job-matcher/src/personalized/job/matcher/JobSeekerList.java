bba644752133730c04f12075b775afb86e6201b9
eem5034 <eem5034@E165-WL10.up.ist.local>
2017-10-10 09:48:09 -0400

    Question class updated.

--- personalized-job-matcher/src/personalized/job/matcher/JobSeekerList.java
+++ personalized-job-matcher/src/personalized/job/matcher/JobSeekerList.java
@@ -13,6 +13,18 @@ public class JobSeekerList {
     }
 
     private void makeUserList() {
+        JobSeeker testUser1 = new JobSeeker ("Test", "User", 1, "password");
+        JobSeeker testUser2 = new JobSeeker ("Test", "User", 2, "password");
+        JobSeeker testUser3 = new JobSeeker ("Test", "User", 3, "password");
+        JobSeeker testUser4 = new JobSeeker ("Test", "User", 4, "password");
+        JobSeeker testUser5 = new JobSeeker ("Test", "User", 5, "password");
+        JobSeeker testUser6 = new JobSeeker ("Test", "User", 6, "password");
         
+        userList.add(testUser1);
+        userList.add(testUser2);
+        userList.add(testUser3);
+        userList.add(testUser4);
+        userList.add(testUser5);
+        userList.add(testUser6);
     }
 }
