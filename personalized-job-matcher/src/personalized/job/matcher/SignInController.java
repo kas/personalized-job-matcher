66f05f2eb1aabcdce219566758c476e6c5d51633
eem5034 <eem5034@E165-WL10.up.ist.local>
2017-10-10 09:39:15 -0400

    Question class updated.

--- personalized-job-matcher/src/personalized/job/matcher/SignInController.java
+++ personalized-job-matcher/src/personalized/job/matcher/SignInController.java
@@ -4,6 +4,17 @@ package personalized.job.matcher;
 
 public class SignInController {
     public SignInController(){
+        //SignInUI loginPrompt = new SignInUI();
+        //JobSeekerList userList = new JobSeekerList();
+    }
+    
+    public void login(){
+        
+    }
+    public void authenticatePass(){
+        
+    }
+    public void authenticateFailed(){
         
     }
 }
