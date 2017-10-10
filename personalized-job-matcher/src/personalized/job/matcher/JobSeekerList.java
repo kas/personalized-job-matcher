d7598e124f9a7f60bb60e5393bfff1b9f5a0d65a
eem5034 <eem5034@E165-WL10.up.ist.local>
2017-10-10 09:44:18 -0400

    Question class updated.

--- personalized-job-matcher/src/personalized/job/matcher/JobSeekerList.java
+++ personalized-job-matcher/src/personalized/job/matcher/JobSeekerList.java
@@ -1,6 +1,18 @@
 package personalized.job.matcher;
 
+import java.util.ArrayList;
+
 
 public class JobSeekerList {
     
+    ArrayList<JobSeeker> userList;
+    
+    public JobSeekerList(){
+        userList = new ArrayList<>();
+        makeUserList();
+    }
+
+    private void makeUserList() {
+        
+    }
 }
