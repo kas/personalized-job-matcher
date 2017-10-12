3e0fa1da547086d00e7a9150eabadb93eac34403
Christian Kim <Christian Kim@10.0.0.121>
2017-10-11 23:57:13 -0400

    Added SignInView and Panel

---
+++ personalized-job-matcher/src/personalized/job/matcher/SignInPanel.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personalized.job.matcher;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class SignInPanel extends JPanel{
+    JLabel user = new JLabel("Username:");
+    JLabel pass = new JLabel("Password:");
+    JTextField userText = new JTextField(20);
+    JTextField passText = new JTextField(20);
+    JButton submit = new JButton("Log In");
+    JList employer = new JList();
+    
+    public SignInPanel()
+    {
+        setLayout(null);
+        setBackground(Color.white);
+        
+        add(user);
+        user.setBounds(100,100,100,50);
+        
+        add(pass);
+        pass.setBounds(100,100,100,100);
+        
+        add(userText);
+        userText.setBounds(200,115,100,20);
+        
+        add(passText);
+        passText.setBounds(200, 140, 100, 20);
+        
+        add(submit);
+        submit.setBounds(400, 125, 100, 25);
+        
+        add(employer);
+        employer.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        employer.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+        employer.setVisibleRowCount(-1);
+        
+        JScrollPane employerScroller = new JScrollPane(employer);
+        employerScroller.setPreferredSize(new Dimension(250,80));
+        
+    }
+}
---
+++ personalized-job-matcher/src/personalized/job/matcher/SignInView.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personalized.job.matcher;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class SignInView extends JFrame{
+    
+    
+    SignInView()
+    {
+        super("Practice");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        SignInPanel sip = new SignInPanel();
+        add(sip);
+        setSize(900,500);
+        setVisible(true);
+    }
+    
+}
