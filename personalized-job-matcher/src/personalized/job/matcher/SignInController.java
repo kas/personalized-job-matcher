763ef46685cc41e118e430f8335238f5e3027602
ckk5093 <ckk5093@E165-WL08.up.ist.local>
2017-10-12 10:12:26 -0400

    Added getText to controller

--- personalized-job-matcher/src/personalized/job/matcher/SignInController.java
+++ personalized-job-matcher/src/personalized/job/matcher/SignInController.java
@@ -10,6 +10,7 @@ public class SignInController {
     
     SignInView loginPrompt;
     
+    
     public SignInController(){
         loginPrompt = new SignInView();
         JobSeekerList userList = new JobSeekerList();
@@ -19,8 +20,11 @@ public class SignInController {
     public void attemptLogin(){
         String tempUserStr = loginPrompt.sip.userText.toString();
         String tempPassStr = loginPrompt.sip.passText.toString();
-        
+        String text = loginPrompt.sip.userText.getText().toString();
+        String text1 = loginPrompt.sip.passText.getText().toString();
+        System.out.println(text + "\n" + text1);
     }
+    
     public void authenticatePass(){
         JobSeekerController jsController = new JobSeekerController();
     }
