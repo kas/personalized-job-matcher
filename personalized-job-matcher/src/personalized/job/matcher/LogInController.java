e6ea3c31a6ad5c5390853c57927c643aeed6c752
ckk5093 <ckk5093@E165-WL07.up.ist.local>
2017-11-02 10:07:41 -0400

    LoginController

---
+++ personalized-job-matcher/src/personalized/job/matcher/LogInController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personalized.job.matcher;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author ckk5093
+ */
+public class LogInController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
