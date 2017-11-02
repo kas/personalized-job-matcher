e3f0ae2b98bada79663f472c4cb83e41b21b4495
Kenneth Schnall <kenneth.schnall@gmail.com>
2017-11-02 09:35:20 -0400

    Create JavaFX project

---
+++ personalized-job-matcher/src/personalized/job/matcher/PersonalizedJobMatcherOld.java
@@ -0,0 +1,11 @@
+
+package personalized.job.matcher;
+
+
+public class PersonalizedJobMatcher {
+	
+	public static void main(String[] args) {
+		SignInController signIn = new SignInController();
+	}
+	
+}
