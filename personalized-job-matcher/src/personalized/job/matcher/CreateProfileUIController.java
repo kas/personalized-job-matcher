501214890076445b159091a7bca8e186bf8353fd
AustinJohnsonCatalyst <tempestcatalyst@gmail.com>
2017-11-05 19:26:56 -0500

    Adding Classes

--- personalized-job-matcher/src/personalized/job/matcher/CreateProfileUIController.java
+++ personalized-job-matcher/src/personalized/job/matcher/CreateProfileUIController.java
@@ -8,6 +8,10 @@ package personalized.job.matcher;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -23,5 +27,24 @@ public class CreateProfileUIController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    @FXML private Text actiontarget;
+    @FXML private TextField username;
+    @FXML private TextField password;
+    @FXML private TextField name;
+    @FXML private TextField age;
+    @FXML private TextField location;
+    @FXML private TextField sex;
+    @FXML private TextField currentJob;
+    
+    @FXML protected void handleSubmitAction(ActionEvent event) {
+        String userText = username.getText();
+        UserProfile newUser = new UserProfile(username.getText(), password.getText(), name.getText(), Integer.parseInt(age.getText()),
+            location.getText(), sex.getText(), currentJob.getText());
+        
+    }
+    
+     @FXML protected void handleCancelAction(ActionEvent event) {
+        actiontarget.setText("Sign in button pressed");
+    }
     
 }
