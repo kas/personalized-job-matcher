aa5f7919d32361eba5102dcf1fc710ac3b830111
elimack3 <32169132+elimack3@users.noreply.github.com>
2017-11-17 16:24:32 -0500

    testquestion, testquestionlist changed

--- personalized-job-matcher/src/personalized/job/matcher/TestQuestionList.java
+++ personalized-job-matcher/src/personalized/job/matcher/TestQuestionList.java
@@ -1,14 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package personalized.job.matcher;
 
-/**
- *
- * @author Krafty
- */
+import java.util.ArrayList;
+
+
 public class TestQuestionList {
+    ArrayList<TestQuestion> testQuestions;
+    
+    public TestQuestionList(){
+        testQuestions = new ArrayList<>();
+        createQuestions();
+        
+    }
+
+    
+
+    private void createQuestions() {
+        TestQuestion question1 = new TestQuestion("Do you enjoy working in teams?");
+        TestQuestion question2 = new TestQuestion("Do you enjoy working in teams?");
+        TestQuestion question3 = new TestQuestion("Do you enjoy working in teams?");
+        TestQuestion question4 = new TestQuestion("Do you enjoy working in teams?");
+        TestQuestion question5 = new TestQuestion("Do you enjoy working in teams?");
+        TestQuestion question6 = new TestQuestion("Do you enjoy working in teams?");
+        TestQuestion question7 = new TestQuestion("Do you enjoy working in teams?");
+        TestQuestion question8 = new TestQuestion("Do you enjoy working in teams?");
+        TestQuestion question9 = new TestQuestion("Do you enjoy working in teams?");
+        TestQuestion question10 = new TestQuestion("Do you enjoy working in teams?");
+        
+        testQuestions.add(question1);
+        testQuestions.add(question2);
+        testQuestions.add(question3);
+        testQuestions.add(question4);
+        testQuestions.add(question5);
+        testQuestions.add(question6);
+        testQuestions.add(question7);
+        testQuestions.add(question8);
+        testQuestions.add(question9);
+        testQuestions.add(question10);
+      
+        
+    }
     
 }
