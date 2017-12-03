f17d7cacb640c44a7ba8fdde550c6bbf7a64a29c
AustinJohnsonCatalyst <tempestcatalyst@gmail.com>
2017-12-03 17:24:01 -0500

    Adding Classes

--- personalized-job-matcher/src/personalized/job/matcher/PersistentDataCollection.java
+++ personalized-job-matcher/src/personalized/job/matcher/PersistentDataCollection.java
@@ -5,10 +5,39 @@
  */
 package personalized.job.matcher;
 
+import java.io.*;
+
 /**
  *
  * @author austi
  */
-public class PersistentDataCollection {
+public class PersistentDataCollection implements Serializable{
     
+    private CareerList theCareerList;
+    private JobSeekerList theJobSeekerList;
+    private PersonalityTestResultsList thePersonalityTestResultsList;
+    private ProfileList theProfileList;
+    private EmployerList theEmployerList;
+    private JobList theJobList;
+    
+    public PersistentDataCollection() {
+        if (theCareerList == null){
+            theCareerList = new CareerList
+        }  
+        if (theJobSeekerList == null){
+            theJobSeekerList = new JobSeekerList
+        }  
+        if (thePersonalityTestResultsList == null){
+            thePersonalityTestResultsList = new PersonalityTestResultsList
+        }  
+        if (theProfileList == null){
+            theProfileList = new ProfileList
+        }  
+        if (theEmployerList == null){
+            theEmployerList = new EmployerList
+        }  
+        if (theJobList == null){
+            theJobList = new JobList
+        }  
+    }   
 }
