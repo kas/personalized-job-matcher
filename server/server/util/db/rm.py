be449b17d42a754545b22452e6db9cf2c5ff364b
Kenneth Schnall <kenneth.schnall@gmail.com>
2017-12-10 10:57:50 -0500

    Remove server files

--- server/server/util/db/rm.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
-
-from config import docker_db_name
-from subprocess import call
-
-call(['docker', 'rm', docker_db_name])
--- server/server/util/db/select_.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
-
-from models import Employer
-
-print('employers:')
-for employer in Employer.select():
-    print(employer.name)
